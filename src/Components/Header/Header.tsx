bff6a98ffe250ec60c4471d2b7716e4a70b7e143
YashAgarwal1201 <legoyashx@gmail.com>
2023-09-19 00:56:06 +0530

    chore: colors update

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,8 +3,10 @@ import { Button } from "primereact/button";
 
 const Header = () => {
   return (
-    <div className="w-full bg-blue-400">
-      <Button icon='pi pi-times' className="bg-black" label='Header' />
+    <div className="w-full p-4 flex justify-between items-center bg-metallic-brown rounded-b-2xl">
+      <Button icon='pi pi-angle-left' className="bg-fern-green text-naples-yellow border-0 rounded-full" />
+      <h1 className="text-naples-yellow">PINEAPPLE</h1>
+      <Button icon='pi pi-question' className="bg-fern-green text-naples-yellow border-0 rounded-full" />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ export default {
     "./src/*.{js,ts,jsx,tsx}"
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'metallic-brown': '#A14712',
+      'ochre': '#C27815',
+      'naples-yellow': '#FFD964',
+      'fern-green': '#557B3F',
+      'bud-green': '#7CB35C'
+    },
     extend: {},
   },
   plugins: [],
