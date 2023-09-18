92e5d6f86879bf1e2c2799e093422f7c1c6e170d
YashAgarwal1201 <legoyashx@gmail.com>
2023-09-18 23:54:16 +0530

    chore: code update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import Header from "./Components/Header/Header";
 import UploadData from "./Pages/UploadData.tsx/UploadData";
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 // import './App.css'
 
 function App() {
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Button } from "primereact/button";
 
 const Header = () => {
   return (
-    <div className='w-full bg-blue-400'>Header</div>
-  )
-}
+    <div className="w-full bg-blue-400">
+      <Button icon='pi pi-times' label='Header' />
+    </div>
+  );
+};
 
-export default Header
+export default Header;
