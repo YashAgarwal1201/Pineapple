0b6c99777fd08b5839da566b72231e7d22dcb296
YashAgarwal1201 <legoyashx@gmail.com>
2023-09-23 23:19:28 +0530

    feat: upload image page

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,10 +3,16 @@ import { Button } from "primereact/button";
 
 const Header = () => {
   return (
-    <div className="w-full p-4 flex justify-between items-center bg-metallic-brown rounded-b-2xl">
-      <Button icon='pi pi-angle-left' className="bg-fern-green text-naples-yellow border-0 rounded-full" />
+    <div className="w-full p-4 flex justify-between items-center bg-metallic-brown rounded-b-2xl shadow-md">
+      <Button
+        icon="pi pi-angle-left"
+        className="bg-fern-green text-naples-yellow border-0 rounded-full"
+      />
       <h1 className="text-naples-yellow">PINEAPPLE</h1>
-      <Button icon='pi pi-question' className="bg-fern-green text-naples-yellow border-0 rounded-full" />
+      <Button
+        icon="pi pi-question"
+        className="bg-fern-green text-naples-yellow border-0 rounded-full"
+      />
     </div>
   );
 };
