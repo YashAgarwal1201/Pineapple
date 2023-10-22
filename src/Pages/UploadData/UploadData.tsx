6598f0fee9af025aa31007bd55a2d85f9f34c513
YashAgarwal1201 <legoyashx@gmail.com>
2023-10-22 20:05:17 +0530

    feat: image upload dialog box

--- src/Pages/UploadData/UploadData.tsx
+++ src/Pages/UploadData/UploadData.tsx
@@ -3,10 +3,12 @@ import Header from "../../Components/Header/Header";
 import { Button } from "primereact/button";
 import { useNavigate } from "react-router-dom";
 import { useAppContext } from "../../AppContext/AppContext";
+import { Dialog } from "primereact/dialog";
 
 const UploadData = () => {
   const navigate = useNavigate();
   const { state, showToast, setSelectedImage } = useAppContext();
+  const [showOptions, setShowOptions] = useState(false);
 
   const uploadHandeler = () => {
     const input = document.createElement("input");
@@ -46,6 +48,7 @@ const UploadData = () => {
           );
         }
         // onHide(!visible);
+        setShowOptions(!showOptions);
       }
     };
     input.click();
@@ -70,10 +73,68 @@ const UploadData = () => {
       <Header />
 
       <div className="h-full p-3 m-3 flex flex-col justify-around items-center bg-metallic-brown rounded-lg shadow-md">
+        {/* <Dialog
+          visible={showOptions}
+          onHide={() => setShowOptions(!showOptions)}
+          header={<div className="text-base md:text-lg lg:text-xl">Choose options</div>}
+          draggable={false}
+          className="w-full md:w-1/2 lg:w-1/3 2xl:w-[500px] absolute md:static bottom-0 md:bottom-auto left-0 md:left-auto"
+        >
+          <div className="flex justify-around items-center">
+            <Button
+              icon="pi pi-upload"
+              label="Browse System"
+              title="Click to browse system"
+              className="flex flex-col items-center gap-y-2 text-sm md:text-base text-naples-yellow bg-fern-green border-fern-green"
+              onClick={() => state.imageSelected.url === "" && uploadHandeler()}
+            />
+            <Button
+              icon="pi pi-camera"
+              label="Capture Image"
+              title="Click to open camera"
+              className="flex flex-col items-center gap-y-2 text-sm md:text-base text-naples-yellow bg-fern-green border-fern-green"
+            />
+          </div>
+        </Dialog> */}
         <div
           className="w-56 sm:w-60 aspect-square p-3 border-2 border-dashed border-naples-yellow rounded-lg cursor-pointer"
-          onClick={() => state.imageSelected.url === "" && uploadHandeler()}
+          onClick={() => {
+            if (state.imageSelected.url === "") setShowOptions(!showOptions);
+          }}
         >
+          {showOptions && (
+            <Dialog
+              visible={showOptions}
+              onHide={() => setShowOptions(!showOptions)}
+              header={
+                <div className="text-base md:text-lg lg:text-xl">
+                  Choose options
+                </div>
+              }
+              draggable={false}
+              className="w-full md:w-1/2 lg:w-1/3 2xl:w-[500px] absolute md:static bottom-0 md:bottom-auto left-0 md:left-auto"
+            >
+              <div className="flex justify-around items-center">
+                <Button
+                  type="button"
+                  icon="pi pi-upload"
+                  label="Browse System"
+                  title="Click to browse system"
+                  className="flex flex-col items-center gap-y-2 text-sm md:text-base text-naples-yellow bg-fern-green border-fern-green"
+                  onClick={() =>
+                    state.imageSelected.url === "" && uploadHandeler()
+                  }
+                />
+                <Button
+                  type="button"
+                  icon="pi pi-camera"
+                  label="Capture Image"
+                  title="Click to open camera"
+                  className="flex flex-col items-center gap-y-2 text-sm md:text-base text-naples-yellow bg-fern-green border-fern-green"
+                />
+              </div>
+            </Dialog>
+          )}
           <div className="w-full h-full flex flex-col justify-center items-center gap-y-4 bg-naples-yellow rounded-md">
             {state.imageSelected.url === "" && (
               <span className="pi pi-image p-4 text-4xl text-metallic-brown bg-bud-green rounded-md"></span>
