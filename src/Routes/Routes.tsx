505819fcfd6ee9a1d766c409eb89e687ea779b69
YashAgarwal1201 <legoyashx@gmail.com>
2023-10-30 00:19:14 +0530

    fix: deployment fixes

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,28 +1,3 @@
-// import React, { lazy } from "react";
-// import {
-//   Routes,
-//   Route,
-//   createBrowserRouter,
-//   createRoutesFromChildren,
-// } from "react-router-dom";
-// import { App } from "./../App";
-
-// const UploadPage = lazy(() => import("./../Pages/UploadData/UploadData"));
-// const PageNotFound = lazy(() => import("./../Pages/PageNotFound/PageNotFound"));
-
-// const RoutesComponent = createBrowserRouter(
-//   createRoutesFromChildren(
-//     // <Route>
-//       <Route path="/" element={<App />}>
-//         <Route path="/" element={<UploadPage />} />
-//         <Route path="*" element={<PageNotFound />} />
-//       </Route>
-//     // </Route>
-//   )
-// );
-
-// export default RoutesComponent;
-
 // import React, { lazy, memo } from "react";
 // import { Routes, Route, Outlet, BrowserRouter } from "react-router-dom";
 // import { App } from "./../App";
