891196d1a6c2632849ef5992fcd79829a7e78bcc
YashAgarwal1201 <legoyashx@gmail.com>
2024-04-11 23:51:53 +0530

    feat: new layout for landing page

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Header from "../Components/Header/Header";
+import { useLocation } from "react-router";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const location = useLocation();
+
   return (
     <div className="w-screen h-[100dvh] relative flex flex-col bg-ochre">
-      <Header />
+      {location.pathname !== "/" && !location.pathname.includes("/success") ? (
+        <Header />
+      ) : (
+        ""
+      )}
       {children}
     </div>
   );
--- src/Services/constants.ts
+++ src/Services/constants.ts
@@ -1,5 +1,7 @@
 import { Label } from "./interfaces";
 
+export const PROJECT_NAME = "Pineapple";
+
 export const DEFAULT_LABEL = "Polygon";
 
 export const labels: Label[] = [
